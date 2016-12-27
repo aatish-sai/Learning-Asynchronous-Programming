7681e9df6f004824c6978b142fae835f161f1c24
Aatish Sai <meatish.sai@gmail.com>
2016-12-27 11:12:10 +0545

    initial commit
    Created a sample program using Async and Await

--- SimpleAsyncAwait/Program.cs
+++ SimpleAsyncAwait/Program.cs
@@ -10,6 +10,39 @@ namespace SimpleAsyncAwait
     {
         static void Main(string[] args)
         {
+            var demo = new AsyncAwait();
+            demo.DoWork();
+
+            while (true)
+            {
+                Console.WriteLine("Doing some work");
+            }
+        }
+    }
+
+    public class AsyncAwait
+    {
+        public async Task DoWork()
+        {
+            
+            await Task.Run(() =>
+            {
+                LongTask();
+            });
+            
+          //  LongTask();
+        }
+
+        private static async Task<string> LongTask()
+        {
+            int counter;
+
+            for(counter = 0; counter < 50000; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+
+            return "counter = " + counter;
         }
     }
 }
